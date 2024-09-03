d9dc1243c77d17b099c6d1b3945c2595f01ef7bb
Bartek <bartlomiej.boczylo@gmail.com>
2024-09-03 16:16:28 +0200

    add new type of routing

--- src/components/ModalWindowComponent.vue
+++ src/components/ModalWindowComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-screen bg-black bg-opacity-50 fixed top-0 left-0 z-20 flex items-center justify-center">
+    <div class="w-full h-screen bg-black bg-opacity-50 fixed top-0 left-0 z-50 flex items-center justify-center">
         <div class="w-1/3 h-3/4 sm:w-10/12 bg-white rounded-2xl p-4">
             <div class="w-full flex flex-row justify-between">
                 <p class="text-xl font-bold">Informacje</p>
