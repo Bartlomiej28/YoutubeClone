d1e6a725b76ee6c3bdfdfe009cd9a40b4a92e71c
Bartek <bartlomiej.boczylo@gmail.com>
2024-09-03 15:55:45 +0200

    add new type of routing

--- src/components/PropositionVideoComponent.vue
+++ src/components/PropositionVideoComponent.vue
@@ -8,18 +8,18 @@
                 <p class="text-sm font-medium truncate">{{ title }}</p>
                 <p class="text-xs text-gray-500">{{ channelTitle }}</p>
             </div>
-
         </div>
     </RouterLink>
 </template>
 
 <script setup>
     import { RouterLink } from 'vue-router';
-    import { useRoute } from 'vue-router';
- 
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
     const props = defineProps(["thumbnail", "title", "channelTitle", "id"]);
 
     const handleGoToNextPage = (id) =>{
-        location.replace(`/video/${id}`)
+        router.push({ name: 'video', params: { id } });
     }
 </script>
