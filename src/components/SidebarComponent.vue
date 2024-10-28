fc8c42907d3b00a78c46f9af2be30d5574d0a3ef
Bartłomiej Boczyło <120282905+Bartlomiej28@users.noreply.github.com>
2024-10-28 10:27:11 +0100

    Update SidebarComponent.vue

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -13,7 +13,6 @@
         </div>
       </RouterLink>
 
-<<<<<<< HEAD
       <button v-for="(button, index) in buttons" :key="index" @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
         <div class="w-[50px] text-2xl">
           <img v-if="button.type === 'image'" :src="button.path" :alt="button.alt" class="max-w-[20px] max-h-[20px]" />
@@ -23,9 +22,7 @@
           {{ button.name }}
         </div>
       </button>
-
       
-
       <hr class="my-2" />
 
       <p class="ml-5">Odkrywaj</p>
@@ -76,180 +73,12 @@
         </div>
       </button>
       <p class="text-center text-sm">Copyright © 2024 - All right reserved by Bartłomiej Boczyło</p>
-=======
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="library" alt="Library" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Biblioteka
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center gap-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <i class='bx bx-history'></i>
-          </div>
-          <div class="text-sm">
-            Historia
-          </div>
-        </button>
-        <hr class="my-2" />
-        <p class="ml-5">Odkrywaj</p>
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="fire" alt="Popular" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Na czasie
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="music" alt="Music" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Muzyka
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="movies" alt="Movies" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Filmy
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="live" alt="Live" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Na żywo
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="games" alt="Games" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Gry
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="news" alt="News" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Wiadomości
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="trophy" alt="Sport" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Sport
-          </div>
-        </button>
-        <hr class="my-2" />
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="viewcanals" alt="View Channels" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Przeglądaj kanały
-          </div>
-        </button>
-        <hr class="my-2" />
-        <p class="ml-5">Więcej z YouTube</p>
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="youtubepremium" alt="Youtube Premium" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            YouTube Premium
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="youtubemusic" alt="Youtube Music" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            YouTube Music
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="youtubekids" alt="Youtube Kids" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            YouTube Kids
-          </div>
-        </button>
-        <hr class="my-2" />
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="settings" alt="Settings" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Ustawienia
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="raporthistory" alt="Raport history" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Historia raportów
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="help" alt="Help" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Pomoc
-          </div>
-        </button>
-  
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="w-[50px] text-2xl">
-            <img :src="sendopinion" alt="Send Opinion" class="max-w-[20px] max-h-[20px]" />
-          </div>
-          <div class="text-sm">
-            Prześlij opinię
-          </div>
-        </button>
-
-        <button @click="handleShowModalWindow" class="flex items-center px-4 bg-transparent border-none w-[90%] mx-auto my-1.5 h-8 rounded-lg hover:bg-gray-200">
-          <div class="text-sm">
-            All rights reserved by Bartłomiej Boczyło
-          </div>
-        </button>
-
-
-          
-      </div>
->>>>>>> fc7966c19a9ba857f80a4d94fcc7e6367bb2bd72
     </div>
   </div>
 </template>
 
-
 <script setup>
 
-<<<<<<< HEAD
 import { RouterLink } from "vue-router";
 import youtubeshorts from "../../public/youtube-shorts-logo-15251.png";
 import subscriptions from "../../public/icons8-subscription-24.png";
@@ -397,10 +226,3 @@ const buttons4 = [
 ]
 
 </script>
-=======
-  
-  const handleShowModalWindow = () => {
-    store.show()
-  };
-</script>
->>>>>>> fc7966c19a9ba857f80a4d94fcc7e6367bb2bd72
